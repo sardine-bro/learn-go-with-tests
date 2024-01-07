73be358f61b1b2e0b1f8d29965a0c5bcf004bae6
Teodoro <tedtke514@gmail.com>
2024-01-06 22:19:58 -0600

    enabled switch case for if statements

--- testingGo/hello.go
+++ testingGo/hello.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 
 }
